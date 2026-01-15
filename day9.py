66447afe693c7eb93f7f9f2514590aa3df7fbfee
Ankush <ankushgowdaa10@gmail.com>
2026-01-15 20:36:18 +0530

    Day 9 : Learned sets

--- day9.py
+++ day9.py
@@ -1,8 +1,8 @@
 #Sets
 
-# #Deduplication
-# nums=[1,2,3,3,4,4,3,22,4,2,3,212,21,1]
-# print(list(set(nums)))
+#Deduplication
+nums=[1,2,3,3,4,4,3,22,4,2,3,212,21,1]
+print(list(set(nums)))
 
 
 #Membership check
@@ -15,27 +15,27 @@ else:
 
 
 
-# #Intersection
-# a = {1, 2, 3, 4}
-# b = {3, 4, 5, 6}
-# print(f"Intersection elements : {a&b}")
-# print(a-b)
-# print(b-a)
+#Intersection
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+print(f"Intersection elements : {a&b}")
+print(a-b)
+print(b-a)
 
 
 
-# #Type behaviour
-# s = {1, True, 1.0, False, 0}
-# print(s)
+#Type behaviour
+s = {1, True, 1.0, False, 0} #1=True=1.0 has same hash and also for false and 0 same hash so it considers as single
+print(s)
 
 
 
-# #Set update
-# permissions = {"read"}
-# permissions.add("write")
-# permissions.add("delete")
-# print(permissions)
-# permissions.discard("read")
-# print(permissions)
+#Set update
+permissions = {"read"}
+permissions.add("write")
+permissions.add("delete")
+print(permissions)
+permissions.discard("read")
+print(permissions)
 
 
