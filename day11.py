81e527e356f251ff1c0396a6dea0b6e36913e573
Ankush <ankushgowdaa10@gmail.com>
2026-02-09 07:46:06 +0530

    Strings

---
+++ day11.py
@@ -0,0 +1,20 @@
+# # Strings
+
+# #1
+# s = "data"
+# s[0] = "D" #Immutable
+
+# #2
+# s = "python"
+# print(s[1:5][::-1]) #Reverse
+
+# #3
+# a = "hello"
+# b = "he" + "llo"
+# print(a is b, a == b) #True True
+
+
+
+
+
+
